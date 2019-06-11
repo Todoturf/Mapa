cec2c2df2d75f290880508a29d64f86420a20cb4
1819ANDROID417 <contacto@todoturf.com>
2019-06-11 19:08:42 +0200

    20190610 21:01 20190611 21:00

--- app/src/main/java/com/example/mapa/LocalizacionActivity.java
+++ app/src/main/java/com/example/mapa/LocalizacionActivity.java
@@ -63,7 +63,7 @@ public class LocalizacionActivity extends AppCompatActivity implements
 
     @Override
     public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Pulsado el botón de localización", Toast.LENGTH_SHORT).show();
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
@@ -71,7 +71,7 @@ public class LocalizacionActivity extends AppCompatActivity implements
 
     @Override
     public void onMyLocationClick(@NonNull Location location) {
-        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Localización actual:\n" + location, Toast.LENGTH_LONG).show();
     }
 
     @Override
